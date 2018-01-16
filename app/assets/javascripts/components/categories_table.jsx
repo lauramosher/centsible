ca4ce12d54b385e3a1a2f6ebadee35c56e048635
Laura Mosher <laura.trevisan@gmail.com>
2018-01-16 10:39:05 -0500

    Add Category
    Handles add and reset of form.
    Handles add errors with error messages (unstyled).

--- app/assets/javascripts/components/categories_table.jsx
+++ app/assets/javascripts/components/categories_table.jsx
@@ -2,28 +2,90 @@ class CategoriesTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      categories: props.categories
+      categories: props.categories,
+      nameError: null,
+      budgetError: null
     };
+    this.handleAddCategory = this.handleAddCategory.bind(this);
+    this.updateState = this.updateState.bind(this);
+  }
+
+  updateState(category) {
+    if (category['category']) {
+      var categories = this.state.categories.slice();
+      categories.push(category['category']);
+      this.setState({categories: categories});
+
+      document.querySelector("form#addCategoryForm").reset();
+      document.querySelector("#addCategoryForm input:first-child").focus();
+    } else {
+      var messages = category['messages'];
+      this.setState({nameError: messages['name'], budgetError: messages['budget']});
+    }
+  }
+
+  handleAddCategory(event) {
+    event.preventDefault();
+    var data = {};
+    var formData = new FormData(event.target);
+    var success = false;
+
+    formData.forEach(function(value, key) {
+      data[key] = value;
+    });
+
+    fetch(this.props.addUrl, {
+      method: "POST",
+      body: JSON.stringify(data),
+      headers: new Headers({
+        'Content-Type': 'application/json'
+      })
+    }).then(
+      function(response) {
+        if (response.statusText === "OK") {
+          success = true;
+        }
+        return response.json();
+      }
+    ).then(
+      data => { this.updateState(data); }
+    )
+
   }
 
   render() {
     return (
-      <table>
-        <thead>
-          <th>Category</th>
-          <th>Budget</th>
-        </thead>
-        <tbody>
-          {
-            this.state.categories.map((category, i) =>
-              <tr>
-                <td>{category.name}</td>
-                <td>{category.budget}</td>
-              </tr>
-            )
-          }
-        </tbody>
-      </table>
+      <section>
+        <table className="category-table">
+          <colgroup>
+            <col />
+            <col />
+            <col />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>Category</th>
+              <th>Budget</th>
+              <th></th>
+            </tr>
+          </thead>
+          <tbody>
+            {
+              this.state.categories.map((category, i) =>
+                <tr key={i}>
+                  <td>{category.name}</td>
+                  <td>{category.budget}</td>
+                  <td></td>
+                </tr>
+              )
+            }
+          </tbody>
+        </table>
+        <NewCategoryForm
+          handler={this.handleAddCategory}
+          nameError={this.state.nameError}
+          budgetError={this.state.budgetError} />
+      </section>
     )
   }
 }
--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -4,4 +4,20 @@ class CategoriesController < ApplicationController
       categories: Category.all
     }
   end
+
+  def create
+    category = Category.create(category_params)
+
+    if category.persisted?
+      render json: { category: category }
+    else
+      render status: 400, json: { messages: category.errors.messages }
+    end
+  end
+
+  private
+
+  def category_params
+    params.require(:category).permit(:name, :budget)
+  end
 end
