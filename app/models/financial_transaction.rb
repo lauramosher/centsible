86dd6ce69e6a30344f8600d8207bd091b0ffc752
Laura Mosher <laura.trevisan@gmail.com>
2018-02-03 14:14:24 -0500

    Add FinancialTransaction validations and tests

--- app/models/financial_transaction.rb
+++ app/models/financial_transaction.rb
@@ -1,2 +1,10 @@
+# frozen_string_literal: true
+
 class FinancialTransaction < ApplicationRecord
+  belongs_to :category
+
+  validates :description, :date, :category_id, presence: true
+  validates :amount, presence: true,
+                     numericality: { greater_than_or_equal_to: 0 }
+  validates :transaction_type, presence: true, inclusion: { in: %w(W D R) }
 end
